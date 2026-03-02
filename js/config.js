477d6e32d425f7c5191a42636ae3cd4991871052
GGS <ggs@GGSs-MacBook-Air.local>
2026-03-02 17:05:45 +0700

    Fix API undefined by including config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// เก็บ URL ของ API ของเราไว้ที่นี่ (ระวังอย่าเอาขึ้น GitHub)
+const CONFIG = {
+    GAS_API_URL: "https://script.google.com/macros/s/AKfycbxTZVzdKVrZAEhbE-bHB-vA_w8Ni2Oo5Gp2M5sNjcTR5rIfgEDMGLIZcr-Z-VWtRokx/exec"
+};
